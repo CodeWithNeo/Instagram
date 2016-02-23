ef1736734a70a046d8fad2b4e7874eb752720d9e
Tarang Khanna <khanna17@purdue.edu>
2016-02-23 16:28:54 -0500

    Setup done

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Initialize Parse
+        // Set applicationId and server based on the values in the Heroku settings.
+        // clientKey is not used on Parse open source unless explicitly configured
+        Parse.initializeWithConfiguration(
+            ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Instagram"
+                configuration.clientKey = "jlhefqblqfbqfhjvhfvhlflqvehqjefvekjfv"
+                configuration.server = "https://instagram2016.herokuapp.com/parse"
+            })
+        )
+        
+        
+        
         return true
     }
 
---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Tarang khanna on 2/23/16.
+//  Copyright © 2016 Tarang khanna. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var userNameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func onSignIn(sender: AnyObject) {
+        let username = userNameField.text ?? ""
+        let password = passwordField.text ?? ""
+        
+        PFUser.logInWithUsernameInBackground(username, password: password) { (user: PFUser?, error: NSError?) -> Void in
+            if let error = error {
+                print("User login failed.")
+                print(error.localizedDescription)
+            } else {
+                print("User logged in successfully")
+                self.performSegueWithIdentifier("loginSegue", sender: nil)
+                // display view controller that needs to shown after successful login
+            }
+        }
+        
+    }
+    
+    
+    @IBAction func onSignUp(sender: AnyObject) {
+        // initialize a user object
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = userNameField.text
+        newUser.email = userNameField.text
+        newUser.password = passwordField.text
+        
+        // call sign up function on the object
+        newUser.signUpInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            if let error = error {
+                print(error.localizedDescription)
+                if error.code == 202 {
+                    print("User name is taken")
+                }
+            } else {
+                print("User Registered successfully")
+                self.performSegueWithIdentifier("loginSegue", sender: nil)
+                // manually segue to logged in view
+            }
+        }
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
