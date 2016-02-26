2e929d5deeaa14eeba6041bbf602db0fb0cbf99e
Tarang Khanna <khanna17@purdue.edu>
2016-02-25 20:34:49 -0500

    load images

--- Instagram/PhotoViewController.swift
+++ Instagram/PhotoViewController.swift
@@ -7,26 +7,57 @@
 //
 
 import UIKit
+import Parse
 
-class PhotoViewController: UIViewController {
+class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var photoData : [PFObject] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        retrieve()
+    }
+    
+    func retrieve() {
+        print("here")
+        let query = PFQuery(className: "UserMedia")
+        query.findObjectsInBackgroundWithBlock { (object:[PFObject]?, error:NSError?) -> Void in
+            if nil != object && object?.count != 0{
+                self.photoData.appendContentsOf(object!)
+                print(object)
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("PhotoTableViewCell") as? PhotoTableViewCell
+        let userMedia = self.photoData[indexPath.row]
+        let media = userMedia.objectForKey("media") as! PFFile
+        let updatedAt = userMedia.updatedAt
+        cell!.insta_photo.image = UIImage(named: "icon")
+        media.getDataInBackgroundWithBlock({ (data:NSData?, error:NSError?) -> Void in
+            if data != nil {
+                cell!.insta_photo.image = UIImage(data: data!)
+            }
+        })
+        return cell!
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.photoData.count
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
-    @IBAction func test(sender: AnyObject) {
-        print("works")
-    }
-    
-
     /*
     // MARK: - Navigation
 
